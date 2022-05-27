201cc0d376b2ab32a502ad51ef9d4f76a45c7611
rashidkhan <rashidkhanbd57@gmail.com>
2022-05-27 20:17:23 +0600

    Question answer

--- src/Components/Page/Blog/Blog.js
+++ src/Components/Page/Blog/Blog.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      <h1>this blog page</h1>
+    <div className='container mx-auto px-4 pt-20'>
+      <div className="">
+        <h1 className="text-2xl py-4">Question answer</h1>
+      </div>
+      <div className="py-2">
+        <strong className='text-slate-700'>1 . How will you improve the performance of a React Application?</strong>
+        <p className='text-slate-500'>ans : Before optimizing a React application, we must understand how React updates its UI and how to measure an app’s performance. This makes it easy to solve any React performance problems.
+
+          Let’s start by reviewing how the React UI updates.</p>
+      </div>
+      <div className="py-2">
+        <strong className='text-slate-700'>What are the different ways to manage a state in a React application?</strong>
+        <p className='text-slate-700'>ans :  There are four main types of state you need to properly manage in your React apps:
+            <ol>
+              <li>1. Local state</li>
+              <li>2. Global state</li>
+              <li>3. Server state</li>
+              <li>4. URL state</li>
+            </ol>
+          </p>
+      </div>
+      <div className="py-2">
+        <strong className='text-slate-700'>How does prototypical inheritance work?</strong>
+        <p className='text-slate-700'>ans : JavaScript is the most common of the prototype-capable languages, and its capabilities are relatively unique. When used appropriately, prototypical inheritance in JavaScript is a powerful tool that can save hours of coding. </p>
+      </div>
+      <div className="py-2">
+        <strong className='text-slate-700'>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</strong>
+        <p className='text-slate-700'> If you write a function component and realize you need to add some state to it, previously you had to convert it to a class. Now you can use a Hook inside the existing function component.  for useing other many components  use by hook</p>
+      </div>
     </div>
   );
 };
